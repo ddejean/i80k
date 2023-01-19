b52dc556a0dc6b94e4ea30a08186426788c58b15
Damien Dejean <dam.dejean@gmail.com>
2023-01-19 10:16:54 +0100

    io: save registers in outb/inb.
    Ensures the registers used in outb/inb are saved and restored by the
    functions to avoid disturbing the callers.

--- cpu.c
+++ cpu.c
@@ -6,9 +6,13 @@ void outb(uint16_t port, uint8_t data) {
 #asm
     push    bp
     mov     bp, sp
+    push    dx
+    push    ax
     mov     dx, [bp+4]
     mov     al, [bp+6]
     out     dx, al
+    pop     ax
+    pop     dx
     pop     bp
 #endasm
 }
@@ -17,9 +21,11 @@ uint8_t inb(uint16_t port) {
 #asm
     push    bp
     mov     bp, sp
+    push    dx
     xor     ax, ax
     mov     dx, [bp+4]
     in      al, dx
+    pop     dx
     pop     bp
 #endasm
 }
